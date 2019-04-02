66b4cb3c8291a8fef9d638488009fb2227edda20
Speedway <harpalassi@gmail.com>
2019-04-02 14:20:38 -0500

    image test

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import image from '../images/mockuper.png';
 
 const Projects = () => {
   return (
     <section id="projects">
       <div className="projects__layout">
         <h1>PROJECTS</h1>
+        <img src={image} width="400px" />
       </div>
     </section>
   );

946d2f3ad0d91da5a8ac3347cc0f8c0e32d3988c
Speedway <harpalassi@gmail.com>
2019-04-03 19:51:00 -0500

    photo test

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import image from '../images/mockuper.png';
 
-const Projects = () => {
-  return (
-    <section id="projects">
-      <div className="projects__layout">
-        <h2>PROJECTS</h2>
-        {/* <img src={image} width="325px" /> */}
-      </div>
-    </section>
-  );
-};
+class Projects extends Component {
+  state = {};
+
+  render() {
+    const project = [{ title: 'All My Books', image: '' }];
+    return (
+      <section id="projects">
+        <div className="projects__layout">
+          <h2>PROJECTS</h2>
+          {/* <img src={image} width="325px" /> */}
+        </div>
+      </section>
+    );
+  }
+}
 
 export default Projects;

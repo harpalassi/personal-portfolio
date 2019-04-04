a5cbe6729b8afe59b8bf75337c14b05b0b97f785
Speedway <harpalassi@gmail.com>
2019-04-03 22:58:13 -0500

    gif testing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Projects from './components/Projects.js';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
-import image from '../images/mockuper.png';
 
 class Projects extends Component {
   state = {};
 
   render() {
-    const project = [{ title: 'All My Books', image: '' }];
+    const project = [
+      {
+        id: 1,
+        title: 'All My Books',
+        image:
+          'https://raw.githubusercontent.com/harpalassi/portfolio-2019/master/src/images/all-my-books.png',
+        description:
+          'All My Books is a full-stack application that searches, saves, and deletes books of interest using the Google Books API. The app is built with React, MongoDB, Express and Node.js.'
+      }
+    ];
     return (
       <section id="projects">
-        <div className="projects__layout">
-          <h2>PROJECTS</h2>
-          {/* <img src={image} width="325px" /> */}
-        </div>
+        <h2>PROJECTS</h2>
+
+        {project.map(project => (
+          <div className="projects__layout">
+            <div className="img__container">
+              <img
+                key={project.id}
+                src={project.image}
+                className="project__img"
+                alt={project.title}
+              />
+            </div>
+            <div className="project__description">
+              <h3>{project.title}</h3>
+              <h4>{project.description}</h4>
+            </div>
+          </div>
+        ))}
       </section>
     );
   }

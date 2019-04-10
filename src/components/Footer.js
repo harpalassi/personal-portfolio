c84c8b871f70c669aa8b486fe2bd3074b4829379
Speedway <harpalassi@gmail.com>
2019-04-10 18:17:47 -0500

    bottom icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,29 @@ const Footer = () => {
     <footer>
       <p>Built with React and CSS Flexbox </p>
       <p>Copyright 2019 © Harpal Assi</p>
+      <div className="bottom-icons">
+        <a
+          href="https://www.linkedin.com/in/harpal-assi/"
+          aria-label="linked-in"
+          target="_blank"
+          rel="noopener noreferrer">
+          <i className="fab fa-linkedin" />
+        </a>
+        <a
+          href="mailto:harpalassi@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="email">
+          <i className="far fa-envelope" />
+        </a>
+        <a
+          href="https://www.github.com/harpalassi"
+          target="_blank"
+          aria-label="github"
+          rel="noopener noreferrer">
+          <i className="fab fa-github-square" />
+        </a>
+      </div>
     </footer>
   );
 };

d1e7f16ee114a5ce117cff5d73d991aba3232698
Speedway <harpalassi@gmail.com>
2019-05-04 13:23:33 -0500

    updated meta desc and minor change to bio

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,14 +15,14 @@ const Header = () => {
         <div className="title">
           <h1>HARPAL ASSI</h1>
           <p>
-            I'm Harpal! I'm a full-stack web developer, tech addict, and musician born in the UK and
+            I'm Harpal! I'm a full-stack web developer, musician, and tech addict born in the UK and
             currently residing in Austin, TX.
           </p>
           <br />
           <p>
             After several years of touring, I found myself falling in love with programming and its
             infinite possibilities to solve real-world problems. I currently work in full-stack
-            technologies such as React, JavaScript, CSS3, HTML5, MySQL, Node.js, Express, and
+            technologies such as React, JavaScript, CSS3, Sass, HTML5, MySQL, Node.js, Express, and
             MongoDB. I'm passionate about building apps that are clean, accessible, and efficient
             for both users and developers alike.
           </p>

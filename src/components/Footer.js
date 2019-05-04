13249e34f705c5d8c21fba579b78ef9c738d8e8c
Speedway <harpalassi@gmail.com>
2019-05-04 16:31:26 -0500

    added bold font

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,33 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
     <footer>
-      <p>Built with React and CSS Flexbox </p>
+      <p>Built with React and Flexbox</p>
       <p>© 2019 Harpal Assi</p>
       <div className="bottom-icons">
         <a
           href="https://www.linkedin.com/in/harpal-assi/"
           aria-label="linked-in"
           target="_blank"
-          rel="noopener noreferrer">
+          rel="noopener noreferrer"
+        >
           <i className="fab fa-linkedin" />
         </a>
         <a
           href="mailto:harpalassi@gmail.com"
           target="_blank"
           rel="noopener noreferrer"
-          aria-label="email">
+          aria-label="email"
+        >
           <i className="far fa-envelope" />
         </a>
         <a
           href="https://www.github.com/harpalassi"
           target="_blank"
           aria-label="github"
-          rel="noopener noreferrer">
+          rel="noopener noreferrer"
+        >
           <i className="fab fa-github-square" />
         </a>
       </div>

f5c92e0912a6de6a0ff5e9e80a4a2fe49b08a8b3
Speedway <harpalassi@gmail.com>
2019-05-23 18:49:38 -0500

    converted profile picture to jpg

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = props => {
       <div className="row-wrap">
         <div className="about__image">
           <img
-            src="https://raw.githubusercontent.com/harpalassi/personal-portfolio/master/src/images/profile-picture.jpg"
+            src="https://raw.githubusercontent.com/harpalassi/personal-portfolio/master/src/images/profile.jpg"
             alt="Portait"
             className="about__picture"
           />

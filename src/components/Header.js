d0bc075a39b38c37a0e872ed1d387d5fc02cfa5d
Harpal <harpalassi@gmail.com>
2019-10-04 00:29:56 -0500

    updated bio

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,54 +1,68 @@
-import React from "react";
-import Resume from "../resume/resume.pdf";
+import React from 'react';
+import Resume from '../resume/resume.pdf';
 
 const Header = props => {
   return (
-    <header id="about">
-      <div className="row-wrap">
-        <div className="about__image">
+    <header id='about'>
+      <div className='row-wrap'>
+        <div className='about__image'>
           <img
-            src="https://raw.githubusercontent.com/harpalassi/personal-portfolio/master/src/images/profile.jpg"
-            alt="Portait"
-            className="about__picture"
+            src='https://raw.githubusercontent.com/harpalassi/personal-portfolio/master/src/images/profile.jpg'
+            alt='Portait'
+            className='about__picture'
           />
         </div>
-        <div className="title">
+        <div className='title'>
           <h1>HARPAL ASSI</h1>
-          <p className="header_paragraph">
-            I'm Harpal! I'm a full-stack web developer, musician, and tech addict born in the UK and
-            currently residing in Austin, TX.
+          <p className='header_paragraph'>
+            I'm Harpal! I'm a full-stack web developer, musician, and tech
+            addict born in the UK and currently residing in Austin, TX.
           </p>
-          <p className="header_paragraph">
-            After several years of touring, I found myself falling in love with programming and its
-            infinite possibilities to solve real-world problems. I enjoy building responsive
-            websites using full-stack technologies such as React, Node.js, Express, MongoDB, MySQL,
-            JavaScript, CSS3, Sass, and HTML5. Lately, I've been having fun with Gatsby and GraphQL as well as contributing to open-source software.
+          <p className='header_paragraph'>
+            After several years of touring, I found myself falling in love with
+            programming and its infinite possibilities to solve real-world
+            problems. I enjoy contributing to open-source and building
+            responsive websites using technologies such as JavaScript, React,
+            Node.js, Express, MongoDB, MySQL, CSS3, SCSS, and HTML5.
           </p>
-          <p className="header_paragraph">
-            {" "}
-            Feel free to check out my{" "}
-            <a href="#projects" aria-label="projects">
+          <p className='header_paragraph'>
+            {' '}
+            Feel free to check out my{' '}
+            <a href='#projects' aria-label='projects'>
               projects
-            </a>{" "}
-            and download my{" "}
-            <a href={Resume} target="_blank" rel="noopener noreferrer" aria-label="resume">
+            </a>{' '}
+            and download my{' '}
+            <a
+              href={Resume}
+              target='_blank'
+              rel='noopener noreferrer'
+              aria-label='resume'
+            >
               resumé
             </a>
-            .{" "}
-          </p>{" "}
-          <p className="header_paragraph">
-            <div id="contact">
-              Contact me below to chat about full-time opportunities, programming, britcoms, raspberry pi, records, or just to say hello.
+            .{' '}
+          </p>{' '}
+          <p className='header_paragraph'>
+            <div id='contact'>
+              Contact me below to chat about full-time opportunities,
+              programming, britcoms, raspberry pi, records, or just to say
+              hello.
             </div>
           </p>
         </div>
       </div>
 
-      <div className="icons">
+      <div className='icons'>
         {props.contactLinks.map(contactLink => {
           const { id, link, name, icon } = contactLink;
           return (
-            <a key={id} href={link} aria-label={name} target="_blank" rel="noopener noreferrer">
+            <a
+              key={id}
+              href={link}
+              aria-label={name}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
               <i className={icon} />
             </a>
           );
